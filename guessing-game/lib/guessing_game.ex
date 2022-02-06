b7fec4955cdab4ff5b2834eb9b7a3fc3241d15e8
tvillegas98 <tvillegas@fi.uba.ar>
2022-02-06 03:01:31 -0300

    Add guessing game solution

---
+++ guessing-game/lib/guessing_game.ex
@@ -0,0 +1,25 @@
+defmodule GuessingGame do
+  def compare(secret_number, guess \\ :no_guess)
+
+  def compare(_, :no_guess) do
+    "Make a guess"
+  end
+
+  def compare(secret_number, secret_number) do
+    "Correct"
+  end
+
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1 do
+    "So close"
+  end
+
+  def compare(secret_number, guess) when secret_number < guess do
+    "Too high"
+  end
+
+  def compare(secret_number, guess) when secret_number > guess do
+    "Too low"
+  end
+
+
+end
